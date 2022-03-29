012828521ee890d28d7c8321eb3d8ce401b6b6aa
a.adli <amirhossein.adli@gmail.com>
2022-03-29 18:27:39 +0430

    created Article model + created ArticleList in views + added Article model to admin.py + created urls for blog app and introduce to config urls

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    publish_datetime = models.DateTimeField(default=timezone.now)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    last_update_datetime = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
