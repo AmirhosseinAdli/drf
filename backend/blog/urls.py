ed4f607d7b4809e8dfd7a9a8add228b5dc3767c4
a.adli <amirhossein.adli@gmail.com>
2022-03-30 15:26:06 +0430

    added a url and view and template for ArticleDetail + created base.html

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ArticleList
+from .views import ArticleList, ArticleDetail
 
 app_name: str = "blog"
 
 urlpatterns: list = [
-    path("", ArticleList.as_view(), name='list')
+    path("", ArticleList.as_view(), name='list'),
+    path("<int:pk>", ArticleDetail.as_view(), name="detail"),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
 from .models import Article
 
 
@@ -7,3 +8,11 @@ from .models import Article
 class ArticleList(ListView):
     def get_queryset(self):
         return Article.objects.filter(status=True)
+
+
+class ArticleDetail(DetailView):
+    def get_object(self, queryset=None):
+        return get_object_or_404(
+            Article.objects.filter(status=True),
+            pk=self.kwargs.get("pk")
+        )
