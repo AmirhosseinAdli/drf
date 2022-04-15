fbbe468624e1e942b044ebdb1525cf002e2af776
a.adli <amirhossein.adli@gmail.com>
2022-04-15 19:35:14 +0430

    commented jwt and uncommented authall + combined jwt and tokenauthentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,11 +39,11 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
-    # 'allauth',
-    # 'allauth.account',
-    # 'allauth.socialaccount',
-    # 'dj_rest_auth',
-    # 'dj_rest_auth.registration',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
 
     'blog.apps.BlogConfig',
     'api.apps.ApiConfig'
@@ -139,11 +139,16 @@ REST_FRAMEWORK: dict = {
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
         # knox
         # oauth
     ]
 }
 
 SITE_ID = 1
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'access'
+JWT_AUTH_REFRESH_COOKIE = 'refresh'
+
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
